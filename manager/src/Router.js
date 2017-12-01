31691a8719df513665a1ebdfc867d4d340c56daf
Huong Do <huong@got-it.co>
2017-12-01 14:41:46 +0700

    Move add button back to top

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -7,69 +7,38 @@ import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
 import EmployeeEdit from './components/EmployeeEdit';
-import { loggoutUser, checkAuth } from './actions';
+import { checkAuth } from './actions';
 import { Confirm } from './components/common'
 
 class RouterComponent extends Component {
-  state = { showModal: false, showLoading: false };
-
   componentDidMount() {
     this.props.checkAuth();
   }
 
-  onLogout() {
-    this.setState({ showModal: true });
-  }
-
-  onConfirmLogout() {
-    this.setState({ showModal: false });
-    this.props.loggoutUser();
-  }
-
-  onDeclineLogout() {
-    this.setState({ showModal: false });
-  }
-
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <Router sceneStyle={{ paddingTop: 65 }}>
-          <Scene key="auth">
-            <Scene key="login" component={LoginForm} title="Please Login" />
-          </Scene>
+      <Router sceneStyle={{ paddingTop: 65 }}>
+        <Scene key="auth">
+          <Scene key="login" component={LoginForm} title="Please Login" />
+        </Scene>
 
-          <Scene key="main" initial={this.props.loggedIn}>
-            <Scene
-              onRight={this.onLogout.bind(this)}
-              rightTitle="Log Out"
-              key="employeeList"
-              component={EmployeeList}
-              title="Employee"
-              initial
-            />
-            <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
-            <Scene key="employeeEdit" component={EmployeeEdit} title="Edit Employee" />
-          </Scene>
-        </Router>
-
-        <Confirm
-          visible={this.state.showModal}
-          onAccept={this.onConfirmLogout.bind(this)}
-          onDecline={this.onDeclineLogout.bind(this)}
-        >
-          Are you sure you want to log out?
-        </Confirm>
-
-      </View>
+        <Scene key="main">
+          <Scene
+            onRight={() => Actions.employeeCreate()}
+            rightTitle="Add"
+            key="employeeList"
+            component={EmployeeList}
+            title="Employee"
+            initial
+          />
+          <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
+          <Scene key="employeeEdit" component={EmployeeEdit} title="Edit Employee" />
+        </Scene>
+      </Router>
     );
   }
 };
 
-const mapStateToProps = state => {
-  return { loggedIn: state.auth.user !== null };
-};
-
-export default connect(mapStateToProps, {
-  loggoutUser,
+export default connect(null, {
   checkAuth,
 })(RouterComponent);
--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import { FlatList, View } from 'react-native';
 import { connect } from 'react-redux';
-import { Actions } from 'react-native-router-flux';
 import _ from 'lodash';
-import { employeesFetch } from '../actions';
+import { employeesFetch, loggoutUser } from '../actions';
 import ListItem from './ListItem';
-import { Button } from './common';
+import { Button, Confirm } from './common';
 
 class EmployeeList extends Component {
+  state = { showModal: false };
+
   componentWillMount() {
     this.props.employeesFetch();
   }
 
-  onAddButtonPress() {
-    Actions.employeeCreate();
+  onLogoutButtonPress() {
+    this.setState({ showModal: true });
+  }
+
+  onConfirmLogout() {
+    this.setState({ showModal: false });
+    this.props.loggoutUser();
+  }
+
+  onDeclineLogout() {
+    this.setState({ showModal: false });
   }
 
   keyExtractor(employee) {
@@ -26,17 +36,26 @@ class EmployeeList extends Component {
 
   render() {
     return (
-      <View style={styles.containerStyle}>
-        <FlatList
-          data={this.props.employees}
-          renderItem={this.renderItem}
-          keyExtractor={this.keyExtractor}
-        />
-      <View style={styles.footerStyle}>
-          <Button onPress={this.onAddButtonPress.bind(this)}>
-            Add New Employee
-          </Button>
+      <View style={{ flex: 1 }}>
+        <View style={styles.containerStyle}>
+          <FlatList
+            data={this.props.employees}
+            renderItem={this.renderItem}
+            keyExtractor={this.keyExtractor}
+          />
+          <View style={styles.footerStyle}>
+            <Button onPress={this.onLogoutButtonPress.bind(this)}>
+              Log Out
+            </Button>
+          </View>
         </View>
+        <Confirm
+          visible={this.state.showModal}
+          onAccept={this.onConfirmLogout.bind(this)}
+          onDecline={this.onDeclineLogout.bind(this)}
+        >
+          Are you sure you want to log out?
+        </Confirm>
       </View>
     );
   }
@@ -62,4 +81,7 @@ const mapStateToProps = state => {
   return { employees };
 };
 
-export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
+export default connect(mapStateToProps, {
+  employeesFetch,
+  loggoutUser
+})(EmployeeList);
